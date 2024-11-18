fe850724ca67039520648d3ad08833ea4fb23042
capapra29 <capapra29@gmail.com>
2024-11-18 19:08:16 +0000

    excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,25 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let whoRandom = who[Math.floor(Math.random() * who.length)];
+
+  let actionRandom = action[Math.floor(Math.random() * action.length)];
+  let whatRandom = what[Math.floor(Math.random() * what.length)];
+  let whenRandom = when[Math.floor(Math.random() * when.length)];
+  console.log(
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom
+  );
+  document.getElementById("excusas").innerHTML =
+    whoRandom + " " + actionRandom + " " + whatRandom + " " + whenRandom;
 };
